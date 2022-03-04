f6366e4c94662c44efd96f736ef52e5ea91bfd10
Joaquin del VAL <delvaljoaquin@gmail.com>
2022-03-04 16:15:04 -0300

    Desafio III Coderhouse

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-success p-4 rounded-3">
+    <h1 class="text-center p-2">DESAFIO III</h1>
+    <h6 class="text-center p-1">Componentes Vue CDN a Vue CLI</h6>
+    <component1 :array-prop="nuevoArray" :boolean-prop="boolean" :style-prop="estilo" class="text-center"></component1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Component1 from "@/components/Component1";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Component1s
+  },
+  data() {
+    return {
+      nuevoArray: [
+        {
+          id: 0,
+          nombre: 'Pepe',
+          apellido: 'Sanchez',
+          edad: 29,
+        },
+        {
+          id: 1,
+          nombre: 'Juanito',
+          apellido: 'Makande',
+          edad: 40,
+        },
+        {
+          id: 2,
+          nombre: 'Frank',
+          apellido: 'Zappa',
+          edad: 52,
+        },
+      ],
+
+      boolean: true,
+      estilo: 'background-color:red; font-family:Oswald; margin-top:25px;'
+    }
   }
+
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Component1.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+  <table v-if="arrayProp.length" :style="styleProp" class="table p-3 rounded-3">
+
+    <tr class="bg-danger">
+      <th>ID</th>
+      <th>NOMBRE</th>
+      <th>APELLIDO</th>
+      <th>EDAD</th>
+      <th>BOOLEAN</th>
+    </tr>
+
+    <tr v-for="persona in arrayProp" :key="persona.id">
+      <td>
+        {{ persona.id }}
+      </td>
+
+      <td>
+        {{ persona.nombre }}
+      </td>
+
+      <td>
+        {{ persona.apellido }}
+      </td>
+
+      <td>
+        {{ persona.edad }}
+      </td>
+
+      <td>
+        {{ booleanProp }}
+      </td>
+
+
+    </tr>
+  </table>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: "Component1",
+  props: {
+    arrayProp: {
+      type: Array,
+      default: () => []
+    },
+    booleanProp: {
+      type: Boolean,
+      default: null
+    },
+    styleProp: {
+      type: String,
+      default: ''
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
